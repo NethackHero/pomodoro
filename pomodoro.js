e39d80f2318d4b01338fd4ea3026cd00ef987146
Tony Tan <tonyyjt@gmail.com>
2016-02-07 21:49:35 +1100

    Pomodoro stop is now working

--- pomodoro.js
+++ pomodoro.js
@@ -1,28 +1,58 @@
 $(document).ready(function(){
 	
-		$('.start').click(startTimer);
+		$('.start').click(studyTimer);
 	//$(".study-txt").prop("value","Hello2");
 	});
 
 var timerRunning = false;
 var sStartTime;
-var startTimer = function(){
-		var studyTime = $(".study-txt").attr("value");
-		var breakTime = $(".break-txt").attr("value");
+var textTime;
+function studyTimer(){
 		if(!timerRunning){
 			timerRunning=true;
 			sStartTime = new Date();
+			textTime = parseInt($(".study-txt").prop("value"));
 		}
 		var curTime = new Date();
 		var diffSec = Math.round((curTime - sStartTime)/1000);
 		
+		var timeLeft = textTime*60 - diffSec;
 		
-		$(".study-txt").prop("value",getDisplayTime(diffSec));
-		window.setTimeout(startTimer, 1000);
+		$(".study-txt").prop("value",getDisplayTime(timeLeft));
+		if(timeLeft > 0){
+			window.setTimeout(studyTimer, 1000);
+		}
+		else{
+			timerRunning = false;
+			breakTimer();
+			$(".study-txt").prop("value",textTime);
+		}
 	};
 	
+var breakTimer = function(){
+	
+	if(!timerRunning){
+		timerRunning = true;
+		sStartTime = new Date();
+		textTime = parseInt($(".break-txt").prop("value"));
+	}
+	var curTime = new Date();
+	var diffSec = Math.round((curTime - sStartTime)/1000);
+	
+	var timeLeft = textTime*60 - diffSec;
+	
+	$(".break-txt").prop("value",getDisplayTime(timeLeft));
+	if(timeLeft > 0){
+		window.setTimeout(breakTimer, 1000);
+	}
+	else{
+		timerRunning = false;
+		studyTimer();
+		$(".break-txt").prop("value",textTime);
+	}
+}
+	
 	var getDisplayTime = function(seconds){
-		
 		var remainingSec = seconds;
 		var hours = Math.floor(remainingSec/3600);
 		remainingSec -= hours*3600;
